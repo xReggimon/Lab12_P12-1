04d4906b8076a8ad838f4359e6f5103353760453
xReggimon <r-mcmullen.1@onu.edu>
2024-04-25 19:19:18 -0400

    Completed Visual Studio project.

--- p12dash1.cpp
+++ p12dash1.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <fstream>
+#include <cctype>
+
 using namespace std;
 
-int count_letters(string str);
-//lower case and upper case
+void pressEnterToContinue();
 
-int count_words(string str);
 
 int main() {
 
-	string line1 = "Moo.";
-	string line2 = "Today is October     26.";
-	string line3 = "I don't give a Holstein for programmers that can only spell a variable one way. (Dr. Stephany)";
-	cout << "Amount of letters for line 1: " << count_letters(line1) << endl;
-	cout << "Amount of letters for line 2: " << count_letters(line2) << endl;
-	cout << "Amount of letters for line 3: " << count_letters(line3) << endl;
+	
+	string datafile;
+	cout << "Enter filename: ";
+	getline(cin, datafile);
+	ifstream inFile(datafile);
 
-	cout << "Amount of words in line 1: " << count_words(line1) << endl;
-	cout << "Amount of words in line 2: " << count_words(line2) << endl;
-	cout << "Amount of words in line 3: " << count_words(line3) << endl;
+	string word;
+	int letters = 0;
+	int words = 0;
 
+	if (!inFile.is_open()) {
+		cerr << "Error opening file." << endl;
+		return 1;
+	}
+	
+	while (inFile >> word) {
+		words++;
+		letters += word.size();
+	}
+
+	inFile.close();
+
+	cout << "Words: " << words << endl;
+	cout << "Letters: " << letters << endl;
+
+	pressEnterToContinue();
 	return 0;
 }
 
-int count_letters(string str) {
-	int count = 0;
-	bool prev_letter = false;
 
-	for (char ch : str) {
-		if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-			count++;
-			prev_letter = true;
-		}
-		else {
-			prev_letter = false;
-		}
-	}
-
-	return count;
-}
-
-int count_words(string str) {
-	int count = 0;
-	istringstream iss(str);
-	string word;
-
-	while (iss >> word) {
-		count++;
-	}
-	return count;
+void pressEnterToContinue() {
+	cout << "\nPress enter to continue... \n";
+	cin.clear();
+	while (cin.get() != '\n')continue;
 }
